31260d7214d28467aabbbdd3c5af12d345db8755
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-10-24 23:49:25 +0300

    Improve tests

--- test/noroutine.js
+++ test/noroutine.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const metatests = require('metatests');
+const metautil = require('metautil');
 const noroutine = require('..');
 const module1 = require('./module1.js');
 
@@ -12,9 +13,20 @@ noroutine.init({
   monitoring: 5000,
 });
 
-metatests.test('Noroutine.init', async (test) => {
-  const res = await module1.method1('value');
-  test.strictSame(res, { key: 'value' });
+metatests.test('Noroutine execute method', async (test) => {
+  const res1 = await module1.method1('value1');
+  test.strictSame(res1, { key: 'value1' });
+
+  await metautil.delay(2000);
+
+  const res2 = await module1.method1('value2');
+  test.strictSame(res2, { key: 'value2' });
+
+  await metautil.delay(2000);
+
+  const res3 = await module1.method1('value3');
+  test.strictSame(res3, { key: 'value3' });
+
   test.end();
-  noroutine.finalize();
+  await noroutine.finalize();
 });
